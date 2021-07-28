fca1ecc76826b8a60d21aa6705e59f12c5ff9bcc
Your Name <you@example.com>
2021-07-28 17:54:46 +0900

    edit managerlogin

--- src/main/java/com/project/starbucks/HomeController.java
+++ src/main/java/com/project/starbucks/HomeController.java
@@ -1,13 +1,13 @@
 package com.project.starbucks;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
-    
+
     @GetMapping("/home")
-    public String home(){
-        return "home";
+    public String home() {
+        return "auth/loginForm";
     }
 }
