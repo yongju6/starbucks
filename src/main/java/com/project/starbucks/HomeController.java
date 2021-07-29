4b5265414fe8f547dfb5a9dcfa73d427a3200a87
DESKTOP-AB63HLR\gahyun <hyunsybm@gmail.com>
2021-07-29 12:16:32 +0900

    base

---
+++ src/main/java/com/project/starbucks/HomeController.java
@@ -0,0 +1,13 @@
+package com.project.starbucks;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
